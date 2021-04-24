c8a6ffac1e39b60e1d4680a7af5e553b13d7962c
zhoujiawei <zhoujiawei@nayuan.net>
2021-04-24 19:49:23 +0800

    feat: 安装标准模板

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -16,6 +16,9 @@ const getProjectTemplate = require('./getProjectTemplate');
 const TYPE_PROJECT = 'project';
 const TYPE_COMPONENT = 'component';
 
+const TEMPLATE_TYPE_NORMAL = 'normal';
+const TEMPLATE_TYPE_CUSTOM = 'custom';
+
 class InitCommand extends Command {
     init() {
         this.projectName = this._argv[0] || '';
@@ -34,46 +37,97 @@ class InitCommand extends Command {
                 this.projectInfo = projectInfo;
                 await this.downloadTemplate();
                 // 3. 安装模板
+                await this.installTemplate();
             }
         } catch (e) {
             log.error(e.message);
         }
     }
 
+    async installTemplate() {
+        log.verbose('templateInfo', this.templateInfo);
+        if (this.templateInfo) {
+            if (!this.templateInfo.type) {
+                this.templateInfo.type = TEMPLATE_TYPE_NORMAL;
+            }
+            if (this.templateInfo.type === TEMPLATE_TYPE_NORMAL) {
+                // 标准安装
+                await this.installNormalTemplate();
+            } else if (this.templateInfo.type === TEMPLATE_TYPE_CUSTOM) {
+                // 自定义安装
+                await this.installCustomTemplate();
+            } else {
+                throw new Error('无法识别项目模板类型！');
+            }
+        } else {
+            throw new Error('项目模板信息不存在！');
+        }
+    }
+
+    async installNormalTemplate() {
+        log.verbose('templateNpm', this.templateNpm);
+        console.log(this.templateNpm.cacheFilePath)
+        // 拷贝模板代码至当前目录
+        const spinner = spinnerStart('正在安装模板...');
+        await sleep();
+        try {
+            const templatePath = path.resolve(this.templateNpm.cacheFilePath, 'template');
+            const targetPath = process.cwd();
+            fse.ensureDirSync(templatePath);
+            fse.ensureDirSync(targetPath);
+            fse.copySync(templatePath, targetPath);
+        } catch (e) {
+            throw e;
+        } finally {
+            spinner.stop(true);
+            log.success('模板安装成功');
+        }
+    }
+
+    async installCustomTemplate() {
+        console.log('安装自定义模板');
+    }
+
     async downloadTemplate() {
-        console.log(this.template, this.projectInfo);
         const { projectTemplate } = this.projectInfo;
         const templateInfo = this.template.find(item => item.npmName === projectTemplate);
         const targetPath = path.resolve(userHome, '.zjw-cli', 'template');
         const storeDir = path.resolve(userHome, '.zjw-cli', 'template', 'node_modules');
         const { npmName, version } = templateInfo;
+        this.templateInfo = templateInfo;
         const templateNpm = new Package({
             targetPath,
             storeDir,
             packageName: npmName,
             packageVersion: version
         });
-        if (!await templateNpm.exists()) {
+        if (!(await templateNpm.exists())) {
             const spinner = spinnerStart('正在下载模板...');
             await sleep();
             try {
                 await templateNpm.install();
-                log.success('下载模板成功');
             } catch (e) {
                 throw e;
             } finally {
                 spinner.stop(true);
+                if (await templateNpm.exists()) {
+                    log.success('下载模板成功');
+                    this.templateNpm = templateNpm;
+                }
             }
         } else {
             const spinner = spinnerStart('正在更新模板...');
             await sleep();
             try {
                 await templateNpm.update();
-                log.success('更新模板成功');
             } catch (e) {
                 throw e;
             } finally {
                 spinner.stop(true);
+                if (await templateNpm.exists()) {
+                    log.success('更新模板成功');
+                    this.templateNpm = templateNpm;
+                }
             }
         }
     }
